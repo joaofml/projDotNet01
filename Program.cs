2ad5f72541c8d3ed2e1dc2add34aab9137bcd0db
joaofml <dproj.joao@gmail.com>
2022-01-17 01:35:28 -0300

    Iniciando repositorio com 3 arquivos

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace meuPrimeiroProjetoDotNet
+{
+
+  public class Program
+  {
+    public static void Main()
+    {
+      string userOption = menu();
+
+      while (userOption.ToUpper() != "S")
+      {
+        try
+        {
+          switch (userOption)
+          {
+            case "1":
+              Console.WriteLine("1()");
+              break;
+            case "2":
+              Console.WriteLine("2()");
+              break;
+            case "3":
+              Console.WriteLine("3()");
+              break;
+            case "4":
+              Console.WriteLine("4()");
+              break;
+            default:
+              throw new Exception("Invalid command");
+          }
+        }
+        catch (Exception)
+        {
+          Console.WriteLine("Opção Invalida");
+        }
+        userOption = menu();
+      }
+      Console.WriteLine("Aplication finished with success");
+
+
+    }
+
+    private static string menu()
+    {
+      Console.WriteLine("1 - Cadastrar aluno");
+      Console.WriteLine("2 - Pesquisar aluno");
+      Console.WriteLine("3 - Atualizar aluno");
+      Console.WriteLine("4 - Deletar aluno");
+      Console.WriteLine("s - Sair ");
+
+      string userOption = Console.ReadLine();
+      return userOption;
+    }
+  }
+
+
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto para praticar .NET C#
+
+## Programa simples apenas com proposito de praticar conceitos de POO e utilizar as ferramentas basicas da linguagem C#
+
+## Para rodar, basta possuir o .NET 6 instalado, e inserir o comando no shell:
+
+## dotnet build ./meuPrimeiroProjetoDotNet.csproj
+
+## dotnet run
