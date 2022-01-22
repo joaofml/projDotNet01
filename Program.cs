10ad67de7f6fb928c7756a1fcd4c868713cb4e5c
joaofml <dproj.joao@gmail.com>
2022-01-21 21:41:32 -0300

    fixed warnings

--- Program.cs
+++ Program.cs
@@ -7,9 +7,8 @@ namespace meuPrimeiroProjetoDotNet
   {
     public static void Main()
     {
-      string userOption = menu();
-
-      while (userOption.ToUpper() != "S")
+      string? userOption = menu();
+      while (userOption?.ToUpper() != "S")
       {
         try
         {
@@ -25,7 +24,6 @@ namespace meuPrimeiroProjetoDotNet
               Console.WriteLine("3()");
               break;
             case "4":
-
               Console.WriteLine("4()");
               break;
             default:
@@ -41,20 +39,16 @@ namespace meuPrimeiroProjetoDotNet
         userOption = menu();
       }
       Console.WriteLine("Aplication finished with success");
-
-
     }
-
-    private static string menu()
+    private static string? menu()
     {
-
       Console.WriteLine("1 - Cadastrar aluno");
       Console.WriteLine("2 - Pesquisar aluno");
       Console.WriteLine("3 - Atualizar aluno");
       Console.WriteLine("4 - Deletar aluno");
       Console.WriteLine("s - Sair ");
 
-      string userOption = Console.ReadLine();
+      string? userOption = Console.ReadLine();
       return userOption;
     }
   }
